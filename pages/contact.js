4858fd53c358d8fc1e15678ffaf9d6d4c881517f
Bennett Davis <bbdavis3@crimson.ua.edu>
2023-02-23 14:46:21 -0600

    Added Contact Page

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,75 @@
 import { Button } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Link from "next/link";
+import Head from "next/head";
+import styles from '../styles/Home.module.css'
+import Image from "next/image";
 
 
 
 
 export default function Contact () {
 
+    const name = "Gio Ramos"
+
     return (
     
-    <div>
-        
-        <body>
-            <div >
-                     <Link href='assets/GiovanniRamos.vcf' download>
-                        <div type="button" class="btn btn-primary btn-sm ">
-                        Add Contact
+        <div>
+            <Head>
+
+            </Head>
+            <main>
+                <div className={styles.body}>
+                    <h1 style={{"font-weight": "bold", textAlign: "center", fontSize: "3rem", padding: "1rem 0rem 0 0", margin: "0"}}>{name}</h1>
+                    <h3 style={{textAlign: "center"}}>Owner</h3>
+                    
+                        <div className={styles.contactImage}>
+                            <Image className={styles.profileImage}
+                                alt="Black Warrior Cleaning Pros Logo"
+                                src="/assets/GioProfile.jpg"
+                                quality={100}
+                                layout="fill"
+                            />
                         </div>
-                    </Link>
-                
-            </div>
-        </body>
+                    
+                    
+                    <div className={styles.buttonSpacing}>
+                        <Link href='assets/GiovanniRamos.vcf' download>
+                            <div type="button" className={styles.addButton}>
+                            Add Contact
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className={styles.iconRow}>
+                        <div className={styles.logoWrapper}>
+                            <Image className={styles.contactLogo}
+                                alt="facebook logo"
+                                src="/assets/fb.png"
+                                quality={100}
+                                layout="fill"
+                            />
+                        </div>
+                        <div className={styles.logoWrapper}>
+                            <Image className={styles.contactLogo}
+                                alt="Instagram Logo"
+                                src="/assets/instawhite.png"
+                                quality={100}
+                                layout="fill"
+                            />
+                        </div>
+                        <div className={styles.logoWrapper}>
+                            <Image className={styles.contactLogo}
+                                alt="TikTok Logo"
+                                src="/assets/ttwhite.png"
+                                quality={100}
+                                layout="fill"
+                            />
+                        </div>
+                    </div>
+                    
+                </div>
+            </main>
         </div>
     )
 

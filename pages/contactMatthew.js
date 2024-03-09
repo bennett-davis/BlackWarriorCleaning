4f588605cfa945a7fc57a0728215d4b1a6e2ddb7
Bennett Davis <bbdavis3@crimson.ua.edu>
2024-03-08 18:57:52 -0600

    Add Matthew Profile

---
+++ pages/contactMatthew.js
@@ -0,0 +1,86 @@
+import { Button } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Link from "next/link";
+import Head from "next/head";
+import styles from '../styles/contact.module.scss'
+import Image from "next/image";
+
+export default function Contact () {
+
+    const name = "Matthew McDaniel";
+    const title = "Maintenance & Service Specialist";
+
+    return (
+    
+        <div>
+            <Head>
+
+            </Head>
+            <main>
+                <div className={styles.body}>
+                    <h1 style={{"font-weight": "bold", textAlign: "center", fontSize: "2.4rem", padding: "1rem 0rem 0 0", margin: "0"}}>{name}</h1>
+                    <h3 style={{textAlign: "center", paddingBottom: "2rem"}}>{title}</h3>
+                    
+                        <div className={styles.contactImage}>
+                          <Image className={styles.profileImage}
+                            alt="Profile Picture of Matthew McDaniel"
+                            src="/assets/MatthewProfile.JPG"
+                            quality={100}
+                            layout="fill"
+                            objectFit="cover"
+                          />
+                        </div>
+                    
+                    
+                    <div className={styles.buttonSpacing}>
+                        <Link href='assets/MatthewMcDaniel.vcf' download>
+                            <div type="button" className={styles.addButton}>
+                              Add Contact
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div className={styles.socialsText}>Check out our social media to see some of our projects!</div>
+                    <div className={styles.iconRow}>
+                        <div className={styles.logoWrapper}>
+                            <a href="https://www.facebook.com/profile.php?id=100087630938358" target="_blank"  rel="noopener noreferrer"> 
+                                <Image className={styles.contactLogo}
+                                    alt="facebook logo"
+                                    src="/assets/fb.png"
+                                    quality={100}
+                                    layout="fill"
+                                />
+                            </a>
+                        </div>
+                
+                        <div className={styles.logoWrapper}>
+                            <a href="https://instagram.com/blackwarriorcleaningpros?igshid=YWJhMjlhZTc=" target="_blank" rel="noopener noreferrer">
+                                <Image className={styles.contactLogo}
+                                    alt="Instagram Logo"
+                                    src="/assets/instawhite.png"
+                                    quality={100}
+                                    layout="fill"
+                                />
+                            </a>
+                        </div>
+                       
+                        <div className={styles.logoWrapper}>
+                            <a href="https://www.tiktok.com/@blackwarriorcleaningpros" target="_blank"  rel="noopener noreferrer">
+                                <Image className={styles.contactLogo}
+                                        alt="TikTok Logo"
+                                        src="/assets/ttwhite.png"
+                                        quality={100}
+                                        layout="fill"
+                                    />
+                            </a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </main>
+        </div>
+    )
+
+
+
+}
